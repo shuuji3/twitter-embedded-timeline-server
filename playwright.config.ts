780867c0341ddc3f12b2d9291f01e3d87f5ea3c3
TAKAHASHI Shuuji <shuuji3@gmail.com>
2023-07-02 13:46:57 +0900

    chore: disable webserver

--- playwright.config.ts
+++ playwright.config.ts
@@ -69,9 +69,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'pnpm start',
-    url: 'http://127.0.0.1:8080',
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: 'pnpm start',
+  //   url: 'http://127.0.0.1:8080',
+  //   reuseExistingServer: !process.env.CI,
+  // },
 })
